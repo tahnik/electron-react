4356a1c0381f03a96a5bca9ce2d58f24a531f8c2
Tahnik Mustasin <tahnik@live.co.uk>
2017-03-12 11:54:22 +0000

    Fixed some questions

--- generators/index.js
+++ generators/index.js
@@ -2,16 +2,6 @@ var Generator = require('yeoman-generator');
 var mkdirp = require('mkdirp');
 
 module.exports = Generator.extend({
-	initialCopy: function() {
-		this.fs.copy(
-			this.templatePath(''),
-			this.destinationPath('electron-react')
-		);
-		this.fs.copy(
-			this.templatePath('.*'),
-			this.destinationPath('electron-react')
-		);
-	},
 	prompts: function() {
 		return this.prompt([
 				{
@@ -24,39 +14,43 @@ module.exports = Generator.extend({
 						type: 'input',
 						name: 'description',
 						message: 'What is your project\'s description?'
-				},
-				{
-						type: 'input',
-						name: 'repository',
-						message: 'What is your project\'s repository URL?'
-				},
-				{
-						type: 'input',
-						name: 'author',
-						message: 'Name of the author of this project:'
 				}
 		]).then((answers) => {
 			this.name = answers.name;
 			this.description = answers.description;
-			this.repository = answers.repository;
-			this.author = answers.author;
 		});
+	},
+	initialCopy: function() {
+		this.fs.copy(
+			this.templatePath(''),
+			this.destinationPath(this.name)
+		);
+		this.fs.copy(
+			this.templatePath('.*'),
+			this.destinationPath(this.name)
+		);
 	},
 	packageJSON: function() {
 		this.fs.copyTpl(
 			this.templatePath('_package.json'), 
-			this.destinationPath('electron-react/package.json'),
+			this.destinationPath(this.name + '/package.json'),
 			{ 
 				name: this.name, 
-				description: this.description, 
-				repository: this.repository, 
-				author: this.author 
+				description: this.description
 			}
 		);
 	},
 	cleanup: function() {
 		this.fs.delete(
-			this.destinationPath('electron-react/_package.json')
+			this.destinationPath(this.name + '_package.json')
 		);
+	},
+	end: function() {
+		var npmDir = process.cwd() + '/' + this.name;
+		process.chdir(npmDir)
+		this.installDependencies({
+			bower: false,
+			npm: true
+		});
 	}
 })
